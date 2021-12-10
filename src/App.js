1a02ca51d96005e0e18546af57bbcdf5c10e999e
Tisoyboy <103482234+Tisoyboy@users.noreply.github.com>
2021-12-10 15:15:59 +0000

    Tests update

--- src/App.js
+++ src/App.js
@@ -11,4 +11,52 @@ import ConsoleView from './ui/ConsoleView';
 import SettingsView from './ui/SettingsView';
 import HueBridgeSelector from './ui/HueBridgeSelector';
 import ActiveBridge from './api/ActiveBridge';
-import React, { Co
+import React, { Component } from 'react';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+
+class App extends Component {
+  onBridgeAuthorizationFailure() {
+    alert(
+      'Please press the link button on the Hue Bridge before trying to connect.',
+    );
+  }
+
+  render() {
+    const activeBridgeId = ActiveBridge.get();
+    return (
+      <BrowserRouter>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">
+              Hue Explorer
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="###"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Bridges
+                  </a>
+        
