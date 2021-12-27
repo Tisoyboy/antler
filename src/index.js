30a2f876b02c504758745eed5d4b216b4776efa7
Tisoyboy <103482234+Tisoyboy@users.noreply.github.com>
2021-12-27 07:41:10 +0000

    update compilerVersion

--- src/index.js
+++ src/index.js
@@ -19,4 +19,56 @@ function oauthFailure(url) {
   alert('OAuth failure');
   window.location.href = url;
   debugger;
-  throw new E
+  throw new Error('OAuth failure');
+}
+
+function oauthRedirect(url, code) {
+  const destinationURL = new URL(url);
+  destinationURL.searchParams.append('code', code);
+  destinationURL.searchParams.append('state', url);
+  window.location.href = destinationURL.href;
+  debugger;
+}
+
+if (window.location.search) {
+  const url = new URL(window.location.href);
+  const code = url.searchParams.get('code');
+  const state = url.searchParams.get('state');
+  if (code && state) {
+    const thisURL = new URL(window.location.href);
+    const thatURL = new URL(state);
+    if (
+      thisURL.protocol !== thatURL.protocol ||
+      thisURL.host !== thatURL.host
+    ) {
+      oauthRedirect(state, code);
+    } else {
+      const settings = Settings.read();
+      const hash =
+        settings.clientId && settings.clientSecret
+          ? btoa(`${settings.clientId}:${settings.clientSecret}`)
+          : null;
+      let bridgeId = null;
+      let bridgeOAuthProperties;
+
+      fetch(`/oauth2/token?code=${code}&grant_type=authorization_code`, {
+        method: 'POST',
+        headers: {
+          Authorization: hash ? `Basic ${hash}` : null,
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+          if (json.fault) {
+            oauthFailure(state);
+          }
+
+          bridgeOAuthProperties = {
+            accessToken: json.access_token,
+            refreshToken: json.refresh_token,
+            tokenType: json.token_type,
+            accessTokenExpiresAt:
+         
