0fa216ccced29859506ae0cf775d02d06a969d66
Tisoyboy <103482234+Tisoyboy@users.noreply.github.com>
2022-01-05 19:45:22 +0000

    Translated using Weblate (Macedonian)

--- src/ui/json/JsonObject.js
+++ src/ui/json/JsonObject.js
@@ -3,4 +3,54 @@
 import React, { Component } from 'react';
 import JsonArray from './JsonArray';
 import JsonNumber from './JsonNumber';
-import JsonString from './
+import JsonString from './JsonString';
+import JsonBoolean from './JsonBoolean';
+import JsonNull from './JsonNull';
+
+import type { Element } from 'React';
+
+class JsonObject extends Component<{
+  depth: number,
+  json: {},
+}> {
+  static defaultProps: {| depth: number, json: { ... } |} = {
+    depth: 0,
+    json: {},
+  };
+
+  render(): Element<'span'> {
+    const currentIndentation = (
+      <pre
+        dangerouslySetInnerHTML={{ __html: '  '.repeat(this.props.depth) }}
+      />
+    );
+    const nextIndentation = (
+      <pre
+        dangerouslySetInnerHTML={{ __html: '  '.repeat(this.props.depth + 1) }}
+      />
+    );
+    return (
+      <span className="jsonObject">
+        {'{'}
+        {Object.keys(this.props.json).length === 0 ? null : (
+          <ul>
+            {Object.keys(this.props.json).map((key, index, keys) => {
+              const value = this.props.json[key];
+              let jsonContent;
+              switch (typeof value) {
+                case 'object':
+                  if (value === null) {
+                    jsonContent = <JsonNull />;
+                    break;
+                  } else if (Array.isArray(value)) {
+                    jsonContent = (
+                      <JsonArray json={value} depth={this.props.depth + 1} />
+                    );
+                    break;
+                  }
+                  jsonContent = (
+                    <JsonObject json={value} depth={this.props.depth + 1} />
+                  );
+                  break;
+                case 'number':
+                  jsonContent =
